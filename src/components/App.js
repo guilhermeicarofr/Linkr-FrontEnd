5b67d54333ac5895f5b2925d7efe662680fb3719
MacenaAndre <andremacenacosta@gmail.com>
2022-10-20 17:54:16 -0300

    feat(LoginContext): Created context folder, LoginContext and LoginContext.Provider

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,22 @@ import SignUp from "./pages/Sign-up";
 import GlobalStyle from "../styles/reset";
 import Timeline from './pages/Timeline.js'
 import SignIn from "./pages/Sign-in";
+import LoginContext from "../contexts/LoginContext";
 
 export default function App() {
+  
+
+
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/timeline" element={<Timeline />} />
-      </Routes>
+      <LoginContext.Provider value={{}}>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/timeline" element={<Timeline />} />
+        </Routes>
+      </LoginContext.Provider>
     </BrowserRouter>
   );
 }
