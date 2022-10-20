b434d212f947443c8a459e8592013bacbc50f179
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-20 00:17:54 -0300

    feat: initial Post component created to test posts rendering

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,24 +2,29 @@ import styled from "styled-components";
 
 function Post ({ postId, url, description, name, userId, picture }) {
 
-
-
-
     return (
-        <div>
+        <PostContainer>
             <header>
                 <img href={picture} alt='' />
                 <h2>{name}</h2>
             </header>
             <p>{description}</p>
             <div>{url}</div>
-        </div>
+        </PostContainer>
     );
 }
-
 export default Post;
 
-
-// const PostContainer = styled.div{
-    
-// }
+const PostContainer = styled.div`   
+    width: 611px;
+    height: 276px;
+    background-color: #171717;
+    border-radius: 16px;
+    margin-bottom: 16px;
+    h2 {
+        color: #FFFFFF;
+    }
+    p {
+        color: #B7B7B7;
+    }
+`;
