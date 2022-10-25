3e8fc3faee3fa40a4535a1e47bc6f63d256f62b1
MacenaAndre <andremacenacosta@gmail.com>
2022-10-25 20:18:15 -0300

    refactor(navbar): layout navbar and searchbar

---
+++ src/styles/searchbar/ContainerSearch.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContainerSearch = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  z-index: 2;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  display: ${(props) => (props.disabled ? "none" : "normal")};
+`;
---
+++ src/styles/searchbar/ResultSearch.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ResultSearch = styled.div`
+  position: fixed;
+  left: 30%;
+  top: -42px;
+  width: 40%;
+  overflow-y: scroll;
+  max-height: 220px;
+  padding-top: 46px;
+  margin-top: 55px;
+  background-color: #e7e7e7;
+  z-index: 3;
+  border-radius: 8px;
+  display: ${(props) => (props.disabled ? "none" : "normal")};
+
+  @media (max-width: 937px) {
+    position: absolute;
+    width: 95%;
+    top: 36px;
+    left: 2.5%;
+  }
+`;
