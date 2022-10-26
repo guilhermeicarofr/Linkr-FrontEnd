54fdf89aee9239765008672853e75c5529a56e20
henriquesdias <henrique.dias1915@gmail.com>
2022-10-26 19:36:34 -0300

    feat: each 15 seconds, a requisition is made, searching new posts. If have new posts, a button is shown. If user clicks on, new posts are render

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -10,10 +10,11 @@ import { Feed, Wrapper } from "../styles/posts/Feed";
 import CreatePost from "../components/post/CreatePost";
 import Trending from "../components/trending/Trending";
 import Post from "../components/post/Posts";
+import LoadingMorePosts from "../components/post/Loading-more-posts";
 
 function Timeline() {
-  const { config,refresh} = useContext(LoginContext);
-  const [posts, setPosts] = useState([]);  
+  const { config, refresh } = useContext(LoginContext);
+  const [posts, setPosts] = useState([]);
   const [message, setMessage] = useState("Loading ...");
 
   useEffect(() => {
@@ -40,7 +41,8 @@ function Timeline() {
             <Title>
               <h2>timeline</h2>
             </Title>
-            <CreatePost/>
+            <CreatePost />
+            <LoadingMorePosts posts={posts} setPosts={setPosts} />
             {posts.length ? (
               posts.map((p, index) => (
                 <Post
@@ -50,7 +52,7 @@ function Timeline() {
                   description={p.description}
                   name={p.name}
                   userId={p.userId}
-                  picture={p.picture}                  
+                  picture={p.picture}
                 />
               ))
             ) : (
