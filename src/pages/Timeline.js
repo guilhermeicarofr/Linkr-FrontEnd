427e49f696efe66bf9af32753c840cd6fd64282c
henriquesdias <henrique.dias1915@gmail.com>
2022-10-27 15:31:12 -0300

    fix: conflits resolved

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -53,12 +53,9 @@ function Timeline() {
                   name={p.name}
                   userId={p.userId}
                   picture={p.picture}
-<<<<<<< HEAD
-=======
                   shareId={p.shareId}
                   shareUserId={p.shareUserId}
-                  shareUserName={p.shareUserName}            
->>>>>>> main
+                  shareUserName={p.shareUserName}
                 />
               ))
             ) : (
