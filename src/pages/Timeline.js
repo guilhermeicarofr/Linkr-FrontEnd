8e6c0f59c0354aa43b8debaca4e721184cde8320
Tissyane <tissyane@gmail.com>
2022-10-28 13:11:15 -0300

    infinite scroll

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-
+import InfiniteScroll from 'react-infinite-scroller';
 import Navbar from "../components/commons/Navbar";
 
 import LoginContext from "../contexts/LoginContext";
@@ -11,26 +11,80 @@ import CreatePost from "../components/post/CreatePost";
 import Trending from "../components/trending/Trending";
 import Post from "../components/post/Posts";
 import LoadingMorePosts from "../components/post/Loading-more-posts";
+import styled from "styled-components";
 
 function Timeline() {
   const { config, refresh } = useContext(LoginContext);
   const [posts, setPosts] = useState([]);
   const [message, setMessage] = useState("Loading ...");
+  const [morePosts, setMorePosts] = useState(true);
+  const [count, setCount] = useState(0);
 
+  // useEffect(() => {
+  //   getTimeline(config)
+  //     .then((res) => {
+  //       setPosts(res.data);
+  //       if (!res.data.length) {
+  //         countProfilesFollowed(config).then((res) => {
+  //           if(res.data?.count < 1) {
+  //             setMessage("You don't follow anyone yet. Search for new friends!");
+  //           } else {
+  //             setMessage("No posts found from your friends");
+  //           }
+  //         })        
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       setMessage(
+  //         "An error occured while trying to fetch the posts, please refresh the page"
+  //       );
+  //     });
+  // }, [refresh, config]);
+  
   useEffect(() => {
-    getTimeline(config)
-      .then((res) => {
-        setPosts(res.data);
-        if (!res.data.length) {
+
+    const fetchData = async () => {
+      try {
+
+    const timeline = await getTimeline(config, {count:0})
+      
+        setPosts(timeline.data);
+        if (!timeline.data.length) {
           setMessage("There are no posts yet");
         }
-      })
-      .catch((error) => {
+      } catch (error) {
         setMessage(
           "An error occured while trying to fetch the posts, please refresh the page"
         );
-      });
+      };
+    }
+
+      fetchData()
   }, [refresh, config]);
+
+
+
+  async function newPosts() {
+  
+    try {
+      const recentPosts = (await getTimeline(config, count)).data;
+     
+
+      setPosts([...posts, ...recentPosts]);
+        if (recentPosts.length === 0) {
+          setMorePosts(false);
+      }
+      setCount(count + 2);
+    } catch (error) {
+     
+      alert(
+        "An error occured while trying to fetch the posts, please refresh the page"
+      );
+    }
+   
+   
+}
+
 
   return (
     <>
@@ -43,7 +97,13 @@ function Timeline() {
             </Title>
             <CreatePost />
             <LoadingMorePosts posts={posts} setPosts={setPosts} />
-            {posts.length ? (
+            <InfiniteScroll
+    pageStart={0}
+    loadMore={newPosts}
+    hasMore={morePosts}
+    loader={<Loading key={0}>Loading ...</Loading>}
+>
+{posts.length ? (
               posts.map((p, index) => (
                 <Post
                   key={index}
@@ -61,6 +121,8 @@ function Timeline() {
             ) : (
               <p>{message}</p>
             )}
+</InfiniteScroll>
+            
           </Feed>
           <Trending />
         </Wrapper>
@@ -70,3 +132,7 @@ function Timeline() {
 }
 
 export default Timeline;
+
+
+const Loading = styled.div `
+color: red`
